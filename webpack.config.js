00985ff608bcf70735848a940eb566e26b555254
Tony <tonywzhang@gmail.com>
2018-08-19 19:19:14 -0700

    First push to github

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// var path = require('path');
+// var webpack = require("webpack");
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./lib/index.js",
+//   output: {
+//     path: path.resolve(__dirname),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /(node_modules)/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: [".js", "*"]
+//   }
+// };
