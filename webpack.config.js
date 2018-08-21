69569fcd98d2c949d36a3fd39089a0d5440dc65d
Tony <tonywzhang@gmail.com>
2018-08-21 16:57:58 -0700

    Working on rendering labels for bar graph

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,9 @@
-// var path = require('path');
-// var webpack = require("webpack");
-//
-// module.exports = {
-//   context: __dirname,
-//   entry: "./lib/index.js",
-//   output: {
-//     path: path.resolve(__dirname),
-//     filename: "bundle.js"
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: [/\.jsx?$/, /\.js?$/],
-//         exclude: /(node_modules)/,
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['es2015']
-//         }
-//       }
-//     ]
-//   },
-//   devtool: 'source-map',
-//   resolve: {
-//     extensions: [".js", "*"]
-//   }
-// };
+const path = require('path');
+
+module.exports = {
+  entry: './lib/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
